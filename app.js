c9d1e7d0cad3170703172df66b61437911f6b7a5
vcdubey <vivekchanddubey598@gmail.com>
2018-01-30 22:17:23 +0530

    Added second header and footer

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(
